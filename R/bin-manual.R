9bab0498f6fdde69e378dfa32bb47b4acf5e7db3
Jose Endrinal <francis.endrinal@gmail.com>
2019-10-04 21:21:36 +0800

    Move interval checking into main function

--- R/bin-manual.R
+++ R/bin-manual.R
@@ -1,36 +1,42 @@
 bin_manual <- function(.data, bad, ..., check = FALSE) {
-  
+
   # Check if .data is data.frame or tibble
-  assert_that(is.data.frame(.data) | is.tbl(.data), 
+  assert_that(is.data.frame(.data) | is.tbl(.data),
               msg = "`.data` is not of type data.frame, tbl, or tbl_df")
-  
+
   # Process bad, get from .data if not input by the user
   bad <- bad_handler(.data, bad)
-  
+
   # Warn if ... is not a list of intervals
   bin_plan <- list2(...)
-  for (i in names(bin_plan)) { 
+  for (i in names(bin_plan)) {
     var <- sym(i)
     result <- rep(NA_character_, nrow(.data))
     for (j in bin_plan[[i]]) {
       # Check if element is in interval then map interval to result
-      target <- map_lgl(.data[[i]], ~interval_contains_element(j, .x))
+      target <- map_lgl(.data[[i]], function(j, .x) {
+        if (j$bounds == "[)") {
+          .x >= j$l & .x < j$r
+        } else if (j$bounds == "(]") {
+          .x > j$l & .x <= j$r
+        }
+      })
       result[target] <- str_c(j$l, " - ", j$r)
     }
-    data_result <- .data %>% 
+    data_result <- .data %>%
       mutate(!!var := result)
-    if (check) { 
-      assert_that(check_bads(data_result, bad, !!var), 
+    if (check) {
+      assert_that(check_bads(data_result, bad, !!var),
                   msg = "Variable should have 30 bad borrowers")
-      assert_that(is_monotonic(check_bads(data_result[[bad]], 
-                                          data_result[[var]])), 
+      assert_that(is_monotonic(check_bads(data_result[[bad]],
+                                          data_result[[var]])),
                   msg = "Variable should have 30 bad borrowers")
     }
     .data <- data_result
   }
-  # Add attributes to data_result before returning 
+  # Add attributes to data_result before returning
   attr(.data, "bad") <- as_string(bad)
-  
+
   # Return binned result
   .data
 }
--- R/interval.R
+++
@@ -1,7 +0,0 @@
-interval_contains_element <- function(cut, value) {
-  if (cut$bounds == "[)") {
-    value >= cut$l & value < cut$r
-  } else if (cut$bounds == "(]") {
-    value > cut$l & value <= cut$r
-  }
-}
