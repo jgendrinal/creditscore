ca99bbee5acf1a5d6ef2f37034fd53918111b49e
Jose Endrinal <francis.endrinal@gmail.com>
2019-10-04 21:12:35 +0800

    Output result from bad_handler

--- R/bad-handler.R
+++ R/bad-handler.R
@@ -1,16 +1,16 @@
 # Handle bad variable, whether input by the user or from the .data object
-bad_handler <- function(.data, bad) { 
+bad_handler <- function(.data, bad) {
   result <- tryCatch(
     {
       bad <- ensym(bad)
-    }, 
+    },
     error = function(cond) {
-      if (is_null(attr(.data, "bad"))) { 
+      if (is_null(attr(.data, "bad"))) {
         abort("`bad` not found in either `.data` or user input.")
-      } else { 
+      } else {
         inform("Getting `bad` variable from `.data`")
         bad <- sym(attr(.data, "bad"))
       }
     })
-  bad
+  result
 }
