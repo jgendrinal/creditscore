4287a4088fc43565874eaf5405988c9b9700274d
Jose Endrinal <francis.endrinal@gmail.com>
2019-10-11 20:57:35 +0800

    Add documentation for bin_manual
    * Add assert_that check for existence of variable name

--- R/bin-manual.R
+++ R/bin-manual.R
@@ -1,3 +1,29 @@
+#' Bin the variables of a dataset manually
+#'
+#' \code{bin_manual} cuts numeric, variables according to the mapping specified
+#' by the user. This is a standard step in feature engineering or data
+#' transformation for scorecard modelling.
+#'
+#' Similar to the dplyr \code{mutate}, the function takes a named list of lists,
+#' separated by commas, with the variable name on the left side, and the
+#' transformation on the right side. \code{bin_manual} uses the \code{c_l} and
+#' \code{c_r} functions to close where the values of a variable will be cut and
+#' transformed into categories.
+#'
+#' @param .data A tibble or data.frame
+#' @param bad A variable name indicating which rows are bad borrowers
+#' @param ... Named lists separated by commas containing mappings of variables
+#'   to a transformation. Cuts for each variable are separated by \code{c_l} or
+#'   \code{c_r}.
+#' @return A tibble or data.frame with the variables transformed into character
+#'   vectors.
+#'
+#' @examples
+#' bin_manual(german, bad, age = c_l(40, 55))
+#' bin_manual(german,
+#'            bad,
+#'            age = c_l(40, 55),
+#'            duration = c_r(15, 35))
 bin_manual <- function(.data, bad, ..., check = FALSE) {
 
   # Check if .data is data.frame or tibble
@@ -10,6 +36,9 @@ bin_manual <- function(.data, bad, ..., check = FALSE) {
   # Warn if ... is not a list of intervals
   bin_plan <- list2(...)
   for (i in names(bin_plan)) {
+
+    # TODO Check if variable name exists in the dataset
+
     var <- sym(i)
     result <- rep(NA_character_, nrow(.data))
     for (j in bin_plan[[i]]) {
