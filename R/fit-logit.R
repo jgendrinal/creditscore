0990b3be63d45d8926d91ded670f690fcdfa2d56
Jose Endrinal <francis.endrinal@gmail.com>
2019-10-15 21:10:07 +0800

    Document fit_logit
    * Pick only variables that are called in the formula to be selected for $`woes`

--- R/fit-logit.R
+++ R/fit-logit.R
@@ -1,4 +1,43 @@
-# TODO Document fit_logit
+#' Fit data into a logistic regression model
+#'
+#' \code{fit_logit} takes a dataset and formula input by the user to generate
+#' a logistic regression model that will be later used for the scoring borrower
+#' accounts.
+#'
+#' The function performs the following:
+#' \enumerate{
+#'     \item Calculate the weights-of-evidence (WOEs) of the categorical
+#'         variables
+#'     \item Replace the category values with the weights of evidence
+#'     \item Returns a glm model object with modified attributes
+#' }
+#'
+#' All numeric variables are used in the logistic regression model as is. All
+#' categorical variables are converted into WOEs before modelling. For better
+#' accuracy and for simplicity, it is recommended that numeric variables are
+#' first \link[=bin_manual]{binned} before modelling.
+#'
+#' @param .data A tibble or data.frame
+#' @param formula A formula of the form \code{y ~ x_1 + x_2 + ...}, where
+#'     \code{y} is the bad variable and \code{x1_2}, \code{x_2}, \code{...} are
+#'     the variables that will be used in the scorecard.
+#' @return A glm object containing the resulting model, with the name of the
+#'     bad variable, and the weights of evidence for each value in each
+#'     variable as attributes.
+#'
+#' @examples
+#' library(dplyr)
+#'
+#' bin_manual(german, bad, age = c_l(40, 55)) %>%
+#'   fit_logit(bad ~ age + history + purpose)
+#'
+#' bin_manual(german,
+#'            bad,
+#'            duration = c_r(15, 32),
+#'            check = TRUE) %>%
+#'  fit_logit(bad ~ duration + age + employed_since)
+#' #
+#'
 #' @import dplyr
 #' @importFrom purrr map map_dbl
 #' @importFrom stats binomial glm var
@@ -38,17 +77,20 @@ fit_logit <- function(.data, formula) {
                 data = df,
                 family = binomial(link = "logit"))
 
+
   # Add attributes, legends to result
   attr(result, "bad") <- as_string(bad)
   result$woes <- bind_cols(select(.data, !!bad),
                            select_if(.data, is.character) %>%
-                             select(!!!target)) %>%
+                             select(one_of(as.character(target)))
+                           # TODO Suppress warnings for this expression
+                           ) %>%
     gather(key = "var", value = "val", -!!bad) %>%
     nest(val, !!bad) %>%
     mutate(woe = map(data, function(data) {
       bad <- as_string(bad)
       calculate_woes(data[["val"]], data[[bad]])
-      })) %>%
+    })) %>%
     select(-data)
 
   # Return model result
