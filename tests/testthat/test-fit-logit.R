5d66958c85b88b65f6003b62f18eece6a5f11871
Jose Endrinal <francis.endrinal@gmail.com>
2020-05-22 05:03:14 +0800

    Block warnings on fit_logit

--- tests/testthat/test-fit-logit.R
+++ tests/testthat/test-fit-logit.R
@@ -1,11 +1,16 @@
 context("Fitting to logistical regression model")
 library(dplyr)
 
+data_binned <- bin_manual(german, bad, duration = c_l(12, 24))
+
 test_that("fit_logit returns glm object", {
 
-  mod <- bin_manual(german, bad, duration = c_l(12, 24)) %>%
-    fit_logit(bad ~ duration)
+  expect_s3_class(fit_logit(data_binned, bad ~ duration), "glm")
 
-  expect_s3_class(mod, "glm")
+})
+
+test_that("fit_logit does not throw warnings or errors", {
+
+  expect_silent(fit_logit(data_binned, bad ~ duration))
 
 })
